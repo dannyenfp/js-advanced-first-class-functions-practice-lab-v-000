2d2fe80d42c1b50cbe2c4f32856dd9ed3c070921
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-25 01:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers){
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, loc){
+  drivers.forEach(function(driver) {
+    if (driver.hometown === loc) {
+    console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue (drivers) {
+  return drivers.slice().sort(function(driver1, driver2) {
+   return driver1.revenue - driver2.revenue; 
+  });
+}
